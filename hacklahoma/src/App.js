280d35d2dfc121352694e2a25ec28b42ed49a709
Anthony Zhou <anthony.zhou03@gmail.com>
2020-02-09 10:37:50 -0600

    Create Dark Mode

--- hacklahoma/src/App.js
+++ hacklahoma/src/App.js
@@ -3,12 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 // import './setup/cloud-styles.css'
 import Form from './Form.js';
-
+import DarkModeToggle from './DarkModeToggle.js';
+import './darkmode.css';
 
 function App() {
   return (
-        <Form></Form>
+    <Container></Container>
   );
+}
+
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cn: "light-mode"
+    }
+  }
+  render() {
+    return (
+      <div className={this.state.cn}>
+          <DarkModeToggle toggleDarkMode={this.toggleDarkMode}></DarkModeToggle>
+          <Form></Form>
+      </div>
+    )
+  }
+
+  toggleDarkMode= isChecked => {
+    if(!isChecked) {
+      this.setState({cn: "dark-mode"});
+    } else {
+      this.setState({cn: "light-mode"});
+    }
+    console.log(this.state.cn);
+  }
 }
 
 export default App;
