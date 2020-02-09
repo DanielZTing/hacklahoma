2fdf70008e0ceeab6c61eac9be55bea7b02a2661
Anthony Zhou <anthony.zhou03@gmail.com>
2020-02-09 09:25:38 -0600

    Changes

--- hacklahoma/src/setup/Step5.js
+++ hacklahoma/src/setup/Step5.js
@@ -11,20 +11,31 @@ function Step5(props) {
         {props.busyTimes.map((entry, index) => 
         <span key={index}>
             <input
+              className="form-control"
+              type="text"
+              name="name"
+              onChange={props.handleBusyName(index)}
+              value={entry.name}
+              />
+            from 
+            <input
+              className="form-control"
               type="time"
               onChange={props.handleStartTime(index)}
               value={entry.startTime}
             />
+            to 
             <input
+              className="form-control"
               type="time"
               onChange={props.handleEndTime(index)}
               value={entry.endTime}
             />
             <button onClick={props.handleDelete(index)}>X</button>
+            <br></br>
           </span>)}
-        <button onClick={props.addQuestion} className="btn">Add another time</button>
+        <button onClick={props.addQuestion} className="btn btn-secondary">Add another time</button>
       </div>
-      <button className="btn btn-success btn-block">Sign up</button>
       </React.Fragment>
     );
   }
