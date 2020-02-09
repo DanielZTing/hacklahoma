a373eb781e1b832c2db70ef4a3aa0851c0594684
Nathaniel Brown <nateabrown@icloud.com>
2020-02-09 09:59:32 -0600

    Re-reveal add another task button

--- hacklahoma/src/Form.js
+++ hacklahoma/src/Form.js
@@ -185,17 +185,17 @@ class Form extends React.Component {
       },
       blockedOff: this.state.busyTimes, // TODO: change startTIme to start and endTime to end, also do this in this.tasks
       Tasks: this.state.tasks
-    }); 
-  //  fetch('https://mywebsite.com/endpoint/', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Accept': 'application/json',
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: jsonString
-  //   })
-
+    });
+  fetch('http://127.0.0.1:5000/query/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: jsonString
+    })
     alert(jsonString)
+
 
     // const { userName, timeOfDay, sleepTime, wakeTime, breakfastStartTime, tasks } = this.state
     // alert(`Your registration detail: \n
